3cb7040d0963904e0d05bfe40bfe66c18563162a
Johnny Domingues <johnnydomingues@sfr.fr>
2024-08-07 22:20:25 +0200

    nettoyage du front + installation database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,31 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+-- Active: 1718631178665@@127.0.0.1@3306@antares
+CREATE TABLE genre (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE poster (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_url VARCHAR(255) NOT NULL,
+    image_alt VARCHAR(255),
+    genre_id INT,
+    FOREIGN KEY (genre_id) REFERENCES genre (id)
 );
+
+INSERT INTO
+    poster (
+        title,
+        description,
+        image_url,
+        artist_id,
+    )
+VALUES (
+        'Chuuuttt',
+        'assets/images/Chuuuttt.jpg',
+        1,
+        5,
+        2.287592,
+        48.862725
+    );
