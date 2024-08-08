a59f8720b450deb2c4e1fffe58651a74e71efceb
Johnny Domingues <johnnydomingues@sfr.fr>
2024-08-08 10:01:35 +0200

    mise en place des models et controllers

--- server/app/services/router.js
+++ server/app/services/router.js
@@ -5,18 +5,18 @@ const router = express.Router();
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
-
+const posterActions = require("../controllers/posterActions");
 // Import item-related actions
-const { browse, read, add } = require("../../../controllers/itemActions");
+// const { browse, read, add } = require("../controllers/itemActions");
 
-// Route to get a list of items
-router.get("/", browse);
+// Route to get a list of posters
+router.get("/", posterActions.browse);
 
 // Route to get a specific item by ID
-router.get("/:id", read);
+// router.get("/:id", read);
 
 // Route to add a new item
-router.post("/", add);
+// router.post("/", add);
 
 /* ************************************************************************* */
 
