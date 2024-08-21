64ca9bd964a9e076b01484719c3a2177c736b874
Johnny Domingues <johnnydomingues@sfr.fr>
2024-08-21 22:22:57 +0200

    ajout bouton log out dans header une fois user connecté

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useLogin } from "../context/LoginContext";
 import "../styles/Header.css";
 import logo from "../assets/Logo.png";
 
 function Header() {
+  const { user, setUser } = useLogin();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setUser(null); // Déconnecte l'utilisateur
+    navigate("/"); // Redirige vers la page d'accueil après déconnexion
+  };
+
   return (
     <header className="header">
       <Link to="/" className="logo">
@@ -13,16 +22,28 @@ function Header() {
           Posters
         </Link>
 
-        <div>
-          <Link to="/login">
-            <p className="header-login-button">Login</p>
-          </Link>
-        </div>
-        <div>
-          <Link to="/register">
-            <p className="header-signUp-button">Sign up</p>
-          </Link>
-        </div>
+        {user ? (
+          <button
+            type="button"
+            className="header-login-button"
+            onClick={handleLogout}
+          >
+            Log Out
+          </button>
+        ) : (
+          <>
+            <div>
+              <Link to="/login">
+                <p className="header-login-button">Login</p>
+              </Link>
+            </div>
+            <div>
+              <Link to="/register">
+                <p className="header-signUp-button">Sign Up</p>
+              </Link>
+            </div>
+          </>
+        )}
       </nav>
     </header>
   );
